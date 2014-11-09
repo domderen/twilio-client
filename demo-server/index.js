8d1dc70a933123470630fdb4753458a3eb0daedb
bhaal275 <dominik.deren@windowslive.com>
2014-11-09 14:52:16 +0100

    - Debugging.

--- demo-server/index.js
+++ demo-server/index.js
@@ -37,5 +37,7 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  console.log(cfg);
+
   console.log('Example app listening at http://%s:%s', host, port);
 });
